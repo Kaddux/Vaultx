b66a99650312ae53479405848f9ab71f8b5bcd18
adars <adarshg352@gmail.com>
2026-06-16 22:07:45 +0530

    its just a table

--- user-service/src/main/java/com/pm/userservice/model/Users.java
+++ user-service/src/main/java/com/pm/userservice/model/Users.java
@@ -1,4 +1,20 @@
 package com.pm.userservice.model;
 
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false,name = "username")
+    private String name;
+
+    @Column(nullable = false,name = "password")
+    private String password;
+
+    @Email
+    @Column(unique = true,nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "registered_date")
+    private LocalDate registered_date;
 }
